7cabb246b417ced7598552b845aec400c7b1782d
Nara Yamini <yamininara92923@gmail.com>
2024-01-23 00:01:34 +0530

    added more springboot concepts and details

---
+++ spring-boot-rest-crud-spring-data-rest/src/main/java/com/example/crud/operation/repo/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.crud.operation.repo;
+
+import com.example.crud.operation.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
